e3104b20bf04bac4ba593e233a05c63eb3200a87
vinay <vinay.madanambeti@zemosolabs.com>
2025-05-12 15:28:12 +0530

    chore: updated the code

--- comp2/src/App.tsx
+++ comp2/src/App.tsx
@@ -1,11 +1,7 @@
 import "./App.css";
 import { Banner } from "./Component2/molecules/Banner";
 function App() {
-  return (
-    <>
-      <Banner />
-    </>
-  );
+  return <Banner />;
 }
 
 export default App;
